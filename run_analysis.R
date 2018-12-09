34158ac6dac8cb3ab966a6e19be6592ae4e9782c
Suman Manandhar <sumankm@hotmail.com>
2018-12-09 18:20:02 +0100

    Load training and test data.

--- run_analysis.R
+++ run_analysis.R
@@ -34,10 +34,41 @@ if(!file.exists(dest.file)) {
 
 # Working data folder
 working.folder <- paste0(data_folder, "UCI HAR Dataset/", sep="")
-print(working.folder)
+# print(working.folder)
 
 # Load needed libraries
-library(dplyr)
+# Loading using lapply is helpful when multiple libraries need to be loaded
+libraries <- c("dplyr")
+lapply(libraries, require, character.only = TRUE)
 
+
+features.file <- paste0(working.folder, "features.txt", sep="")
+activity.labels.file <- paste0(working.folder, "activity_labels.txt", sep="")
+
+### Load feature names and activity labels
+feature_names <- read.table(features.file, header = F)
+activity_labels <- read.table(activity.labels.file, header = F,
+                              col.names = c("activity_id", "activity_name"))
+
+# Load training data
+X_train.file <- paste0(working.folder, "train/X_train.txt", sep="")
+y_train.file <- paste0(working.folder, "train/y_train.txt", sep="")
+subject_train.file <- paste0(working.folder, "train/subject_train.txt", sep="")
+
+train_X <- read.table(X_train.file, header = F)
+train_y <- read.csv(y_train.file, header = F, col.names = c("activity_id")) %>%
+                left_join(activity_labels, by = "activity_id")
+train_subject <- read.csv(subject_train.file, header = F, col.names = "subject")
+
+
+# Load test data
+X_test.file <- paste0(working.folder, "test/X_test.txt", sep="")
+y_test.file <- paste0(working.folder, "test/y_test.txt", sep="")
+subject_test.file <- paste0(working.folder, "test/subject_test.txt", sep="")
+
+test_X <- read.table(X_test.file, header = F)
+test_y <- read.csv(y_test.file, header = F, col.names = c("activity_id")) %>%
+                left_join(activity_labels, by = "activity_id")
+test_subject <- read.csv(subject_test.file, header = F, col.names = "subject")
 
 
